b37338e461ba25fc2da6fea57a6d5d1ea023e7ca
Daniil Khudiakov <khudiakov@outlook.com>
2022-04-16 14:40:16 +0200

    add data fixtures

--- src/components/orderbook.stories.tsx
+++ src/components/orderbook.stories.tsx
@@ -1,18 +1,24 @@
-import { Orderbook } from './orderbook'
+import { OrderbookComponent } from './orderbook'
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import FIXTURE from "./orderbook.fixture.json";
+
 export default {
     title: 'Orderbook',
-    component: Orderbook,
-} as ComponentMeta<typeof Orderbook>;
+    component: OrderbookComponent,
+} as ComponentMeta<typeof OrderbookComponent>;
 
-const Template: ComponentStory<typeof Orderbook> = (args) => <div id="app"><Orderbook {...args} /></div>
+const Template: ComponentStory<typeof OrderbookComponent> = (args) => <div id="app"><OrderbookComponent {...args} /></div>
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-    productId: 'PI_ETHUSD',
+    ready: FIXTURE.ready,
+    spread: FIXTURE.spread,
+    spreadPercentage: FIXTURE.spreadPercentage,
+    asks: FIXTURE.asks,
+    bids: FIXTURE.bids,
     onToggle: () => undefined
 };
 Primary.parameters = {
